c2bbf35f69df9c7f90e4a472879080dd76f2c1b1
Selinayujia <36021325+Selinayujia@users.noreply.github.com>
2023-04-29 22:08:46 -0700

    progress

--- Hw2/LinearRegression.py
+++ Hw2/LinearRegression.py
@@ -2,7 +2,7 @@ import numpy as np
 from tqdm import tqdm
 
 class Linear_Regression():
-    def __init__(self, alpha = 1e-3 , num_iter = 10000, early_stop = 100, 
+    def __init__(self, alpha = 1e-3 , num_iter = 10000, early_stop = 100,
         intercept = True, init_weight = None, penalty = None, 
         lam = 1e-3, normalize = False, adaptive=True):
         """
@@ -42,28 +42,33 @@ class Linear_Regression():
         
         self.max = np.zeros((1, X.shape[1]))
         self.min = np.zeros((1, X.shape[1]))
+        
         for i in range(self.X.shape[1]):
-            self.max[:, i] = self.X[:, i].max()
-            self.min[:, i] = self.X[:, i].min()
+            self.max[:, i] = self.X[:, i].max() #[[996]]  column expression
+            self.min[:, i] = self.X[:, i].min() #[[1]]
+        
 
         ############### START TODO 1 ###############
         # Normalize the data using the formula provided in lecture
-        if self.normalize:            
-            raise NotImplementedError
-            self.X = ...
+        if self.normalize:
+            self.X = (self.X - self.min)/(self.max - self.min)
+ 
+            
         ############### END TODO 1 ###############
 
         ############### START TODO 2 ###############
         # Add bias (if necessary) by concatanating a constant column into X
         # Hint: go through HW1 Q5 might be helpful
         if self.intercept:
-            raise NotImplementedError
-            self.X = ...
+            intercept_col = np.ones((self.X.shape[0], 1))
+            self.X = np.concatenate((self.X, intercept_col), axis=1)
+            
         ############### END TODO 2 ###############
 
         # initialize coefficient
         self.coef = self.init_weight if self.init_weight is not None\
         else np.array([np.random.uniform(-1,1,self.X.shape[1])]).T
+        
 
         # start training, self.loss is used to record losses over iterations
         self.loss = []
@@ -71,25 +76,30 @@ class Linear_Regression():
         
     def gradient(self):
         coef = -2 / len(self.X)
+   
 
         ############### START TODO 3 ###############
         # Find prediction and gradient
         # Hint: Find the model's prediction from the given inputs with the 
         #       coefficient, then calculate the gradient
         # If you forgot the formula, find them in lecture 4 and 5
-        raise NotImplementedError
-        grad = ...
+        
+        #print('predicted',(np.matmul(self.X, self.coef)-self.y))
+       # print('mul',np.dot((np.matmul(self.X, self.coef)-self.y),self.X))
+        grad = coef * np.matmul(self.X.T, (np.matmul(self.X, self.coef)-self.y))
+        
+        grad = grad/max(grad)
+        
+
         ############### END TODO 3 ###############
 
         ############### START TODO 4 ###############
         # Implement regularization penalty
         # Hint: Use self.lam
         if self.penalty == 'l2':
-            raise NotImplementedError
-            grad += ...
+            grad += 2 * self.lam * self.coef
         elif self.penalty == 'l1':
-            raise NotImplementedError
-            grad += ...
+            grad += self.lam * np.sign(self.coef)
         else: 
             pass
         ############### END TODO 4 ###############
@@ -99,64 +109,55 @@ class Linear_Regression():
     def gradient_descent(self):
         print('Start Training')
         for i in range(self.num_iter):
-
-            ############### START TODO 5 ###############
-            raise NotImplementedError
-            # calculate prediction y based on current coefficients (self.coef)                              
-            previous_y_hat = ...
+            previous_y_hat = np.matmul(self.X,self.coef)
+            print(previous_y_hat-self.y)
             grad = self.gradient()
-            # calculate the new coefficients after incorporating the gradient
-            temp_coef = ...
-            ############### END TODO 5 ###############
+            temp_coef = self.coef + (self.alpha * grad)
+            
+           # print('prev',self.coef, 'curr',temp_coef)
+            #print('------------                     ')
 
-            ############### START TODO 6 ###############
-            # calculate regularization cost (alias: regularization loss) based on
-            # self.coef and temp_coef
-
-            raise NotImplementedError
+            
             if self.penalty == 'l2':
-                previous_reg_cost = ...
-                current_reg_cost = ...
+                previous_reg_cost = 2 * self.lam * self.coef
+                current_reg_cost = 2 * self.lam * temp_coef
             elif self.penalty == 'l1':
-                previous_reg_cost = ...
-                current_reg_cost = ...
+                previous_reg_cost = self.lam * np.sign(self.coef)
+                current_reg_cost = self.lam * np.sign(temp_coef)
             else:
                 previous_reg_cost = 0
                 current_reg_cost = 0
-            ############### END TODO 6 ###############
+            
+            
+  
 
-            ############### START TODO 7 ###############
-            # Calculate error (alias: loss) using sum squared loss 
-            # and add regularization cost
-            raise NotImplementedError
-            pre_error = np.sum(np.square(...)) + previous_reg_cost
-            current_error = np.sum(np.square(...)) + current_reg_cost
-            ############### END TODO 7 ###############
+            pre_error = np.sum(np.square(self.y - previous_y_hat)) + previous_reg_cost
+            current_error = np.sum(np.square(self.y - np.matmul(self.X, temp_coef))) + current_reg_cost
+            
+            self.coef = temp_coef
+            
+            '''
 
-            # Early Stop: early stop is triggered if loss is not decreasing 
-            # for some number of iterations
+
+            if current_error < pre_error:
+                self.alpha *= 1.3  if self.adaptive else self.alpha
+                self.coef = self.coef - (self.alpha * grad)
+            else:
+                self.alpha *= 0.9 if self.adaptive else self.alpha
+                self.coef = temp_coef
+                
+            '''
+                
+            self.loss.append(float(current_error))
+                        
             if len(self.loss) > self.early_stop and \
             self.loss[-1] >= max(self.loss[-self.early_stop:]):
-                print('-----------------------') 
+                print('-----------------------')
                 print(f'End Training (Early Stopped at iteration {i})')
+                print(self.coef)
                 return self
-            
-            ############### START TODO 8 ###############
-            # Implement adaptive learning rate
-
-            # Rules: if current error is smaller than previous error, 
-            # multiply the current learning rate by 1.3 and update coefficients, 
-            # otherwise by 0.9 and do nothing with coefficients
-            raise NotImplementedError
-            if current_error < pre_error:
-                self.alpha = ... if self.adaptive else self.alpha
-                ...
-            else:
-                self.alpha = ... if self.adaptive else self.alpha
-            ############### END TODO 8 ###############
-
-            # record stats
-            self.loss.append(float(current_error))
+                
+           
             if i % 1000000 == 0:
                 print('-----------------------')
                 print('Iteration: ' +  str(i))
@@ -174,12 +175,15 @@ class Linear_Regression():
         ############### START TODO 9 ###############
         # add bias (if necessary, same as TODO 2)
         if self.intercept:
-            raise NotImplementedError
-            X = ...
+
+            intercept_col = np.ones((self.X.shape[0], 1))
+            X = np.concatenate((X, intercept_col), axis=1)
+        
+            
 
         # Find the model's predictions
         # Hint: Use matrix multiplication ('@' might come in handy here)
-        y = ...
+        y = np.matmul(X,self.coef)
         return y
         ############### END TODO 9 ###############
 
